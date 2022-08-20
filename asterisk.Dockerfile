374488f6f992120c27574eab9798644d2c2e74ce
Luiz Braga <btmluiz@outlook.com>
2022-08-19 22:30:52 -0300

    Production settings v2

--- asterisk.Dockerfile
+++ asterisk.Dockerfile
@@ -1,3 +1,9 @@
 FROM btmluiz/asterisk:18-lts
 
 COPY ./conf/ /etc/asterisk/
+
+COPY ./docker-entrypoint.asterisk.sh /docker-entrypoint.sh
+
+ENTRYPOINT ["/bin/bash", "/docker-entrypoint.sh"]
+
+CMD ["asterisk", "-mqf"]
--- docker-entrypoint.asterisk.sh
+++ docker-entrypoint.asterisk.sh
@@ -15,3 +15,24 @@ sed -i "s/#DB_PORT#/$ASTERISK_DATABASE_PORT/g" /etc/asterisk/*.conf
 sed -i "s/#DB_USER#/$ASTERISK_DATABASE_USER/g" /etc/asterisk/*.conf
 sed -i "s/#DB_PASSWORD#/$ASTERISK_DATABASE_PASSWORD/g" /etc/asterisk/*.conf
 sed -i "s/#DB_NAME#/$ASTERISK_DATABASE/g" /etc/asterisk/*.conf
+
+set -eo pipefail
+shopt -s nullglob
+
+if [[ "${1:0:1}" = '-' ]]; then
+    set -- asterisk "$@"
+fi
+
+chown -R asterisk:asterisk /var/lib/asterisk/db
+chown -R asterisk:asterisk /var/log/asterisk
+chown -R asterisk:asterisk /var/spool/asterisk
+
+if [[ "${1}" = "asterisk" ]]; then
+    if [[ "$(id -u)" = "0" ]]; then
+        exec gosu asterisk "$@"
+    fi
+
+    exec asterisk "$@"
+fi
+
+exec "$@"
